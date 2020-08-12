4ade99fa4ac25a8d6792f324bdd28616045eed9d
Chen Sicong <sicong.chen@163.com>
2020-08-12 18:22:47 +0800

    [Chen Sicong] exercise-4

--- src/exercise-4/components/App.js
+++ src/exercise-4/components/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {BrowserRouter as Router, Redirect, Switch} from 'react-router-dom';
 import {Route} from "react-router";
 import Home from "./Home";
 import About from "./About";
@@ -11,10 +11,12 @@ class App extends Component {
     return (
       <div className="app">
         <Router>
-            <Route component={NotMatch} />
-            <Route path='/' component={Home} />
-            <Route path='/:user' component={User} />
-            <Route path='/about' component={About} />
+            <Switch>
+                <Route exact path='/' component={Home} />
+                <Route path='/:user(\d+)' component={User} />
+                <Route path='/about' component={User} />
+                <Route component={NotMatch} />
+            </Switch>
         </Router>
       </div>
     );
